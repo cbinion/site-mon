94d288f23db277468da7c33a3cc98e47730c209e
cbinion <134014495+cbinion@users.noreply.github.com>
2026-09-12 19:45:25 -0500

    Add HTTP availability check to run_checks

--- src/site_mon/monitor.py
+++ src/site_mon/monitor.py
@@ -1,6 +1,8 @@
 import ssl
 from datetime import UTC, datetime
 
+import requests
+
 from site_mon.cert import fetch_cert
 
 
@@ -11,6 +13,11 @@ def run_checks(target: dict):
 
     try:
         cert_info = fetch_cert(hostname, port)
+        status_code, response_time = make_https_request(hostname, port)
+
+        print(
+            f"Expected status: {expect_status} - Actual status: {status_code} - Elapsed request time: {response_time}"
+        )
 
         print(cert_info.issuer)
         print(cert_info.not_before)
@@ -22,3 +29,8 @@ def run_checks(target: dict):
         print(f"There was an error with the certificate: {e.verify_message}")
     except OSError as e:
         print(f"There was an error reaching the hostname: {e}")
+
+
+def make_https_request(hostname: str, port: int) -> tuple[int, float]:
+    response = requests.get(f"https://{hostname}:{port}", timeout=10)
+    return response.status_code, response.elapsed.total_seconds()
